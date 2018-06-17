b4dc83cdd9b029c3f34fc87e5534617f630af2c6
AHRL <527919996@qq.com>
2018-06-17 23:41:26 +0800

    侧边栏以及整体设计

--- ui/src/components/mainModule/personalCenter/index.js
+++ ui/src/components/mainModule/personalCenter/index.js
@@ -1,10 +1,28 @@
 export default {
+    data() {
+        return {
+            isCollapse: false,
+            form: {
+                name: '',
+                region: '',
+                date1: '',
+                date2: '',
+                delivery: false,
+                type: [],
+                resource: '',
+                desc: ''
+            }
+        }
+    },
     methods: {
         handleOpen(key, keyPath) {
             console.log(key, keyPath);
         },
         handleClose(key, keyPath) {
             console.log(key, keyPath);
+        },
+        onSubmit() {
+            console.log('submit!');
         }
     }
 }
