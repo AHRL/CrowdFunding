063309916ebb3ede2a919f7bc6c3268b2b6ee9eb
AHRL <527919996@qq.com>
2018-07-02 11:27:45 +0800

    progress

--- ui/src/components/mainModule/manageCenter/children/survey.vue
+++ ui/src/components/mainModule/manageCenter/children/survey.vue
@@ -1,24 +1,50 @@
 <template>
     <div class="survey">
-        <el-row>
-            <el-col class="item" :span="6">
+        <el-row class="allProgress">
+            <el-col class="circleProgress" :span="5">
                 <div>
-                    <el-progress :show-text="false" stroke-width="15" type="circle" :percentage="80" color="#8e71c7"></el-progress>
+                    <el-progress :show-text="false" :stroke-width="15" type="circle" :percentage="80" color="#409EFF"></el-progress>
                 </div>
             </el-col>
-            <el-col class="item" :span="6">
+            <el-col class="circleProgress" :span="5">
                 <div>
-                    <el-progress :show-text="false" stroke-width="15" type="circle" :percentage="80" color="#8e71c7"></el-progress>
+                    <el-progress :show-text="false" :stroke-width="15" type="circle" :percentage="80" color="#8e71c7"></el-progress>
                 </div>
             </el-col>
-            <el-col class="item" :span="6">
+            <el-col class="progress" :span="14">
                 <div>
-                    <el-progress :show-text="false" stroke-width="15" type="circle" :percentage="80" color="#8e71c7"></el-progress>
-                </div>
-            </el-col>
-            <el-col class="item" :span="6">
-                <div>
-                    <el-progress :show-text="false" stroke-width="15" type="circle" :percentage="80" color="#8e71c7"></el-progress>
+                    <el-row>
+                        <el-col :span="4">
+                            <span>月增长</span>
+                        </el-col>
+                        <el-col :span="20">
+                            <el-progress :percentage="40"></el-progress>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col :span="4">
+                            <span>月增长</span>
+                        </el-col>
+                        <el-col :span="20">
+                            <el-progress :percentage="100" color="#8e71c7"></el-progress>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col :span="4">
+                            <span>月增长</span>
+                        </el-col>
+                        <el-col :span="20">
+                            <el-progress :percentage="60" color="#dc5771"></el-progress>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col :span="4">
+                            <span>月增长</span>
+                        </el-col>
+                        <el-col :span="20">
+                            <el-progress :percentage="10" color="#da9c63"></el-progress>
+                        </el-col>
+                    </el-row>
                 </div>
             </el-col>
         </el-row>
@@ -34,11 +60,23 @@ export default {
 }
 </script>
 <style scoped>
-.item{
+.allProgress{
+    background: #fff;
+    margin: 10px;
+}
+.allProgress .el-col{
     padding: 10px;
 }
-.item div{
+.circleProgress div{
     text-align: center;
-    background: #fff;
+}
+.progress>div{
+    border-left: 1px solid rgb(235, 233, 233);
+    font-size: 14px;
+    color: black;
+    padding-left: 20px;
+}
+.progress .el-progress{
+    
 }
 </style>
